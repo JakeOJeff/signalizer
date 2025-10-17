89f8ecb357247835ba5a17afef8d82e167380fa6
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 00:40:41 +0530

    Fixed flexbox grid and cleaned data

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center m-10">
+      <main className="bg-gray-900 flex flex-col items-center justify-center min-h-screen w-full">
         <div>
-          <h1 className="text-6xl p-10">Signalizer</h1>
-        </div>
-        <div className="flex flex-row justify-between ">
-          <div className="rounded-2xl mr-2 outline-3 outline-gray-500 bg-gray-600 p-4 pr-6 pl-6 text-xl font-bold cursor-pointer duration-500 hover:bg-gray-700" >Create Signal</div>
-          <div className="rounded-2xl ml-2 outline-3 outline-gray-300 text-gray-800 bg-gray-100 p-4 pr-6 pl-6 text-xl font-bold cursor-pointer duration-500 hover:bg-gray-300" >Decode Signal</div>
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-7xl my-4">Signalizer</h1>
+            <h3 className="text-2xl mb-15">Create and receive encodeded signals with just a click!</h3>
+          </div>
+          <div className="flex flex-row justify-center items-center">
+            <div className="rounded-2xl mr-2 outline-3 outline-gray-500 bg-gray-600 p-4 pr-6 pl-6 text-xl font-bold cursor-pointer duration-500 hover:bg-gray-700" >Create Signal</div>
+            <div className="rounded-2xl ml-2 outline-3 outline-gray-300 text-gray-800 bg-gray-100 p-4 pr-6 pl-6 text-xl font-bold cursor-pointer duration-500 hover:bg-gray-300" >Decode Signal</div>
+          </div>
         </div>
       </main>
     </>
