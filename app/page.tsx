5f0bdcd7e15d4bfce9fa62f26a9f369262b69930
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 00:17:10 +0530

    Created basic text matter

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 
 export default function Home() {
   return (
-    <main>
-      <div></div>
-      <div>
-        <div></div>
-        <div></div>
-      </div>
-    </main>
+    <>
+      <main className="flex flex-col items-center justify-center m-10">
+        <div>
+          <h1 className="text-6xl p-10">Signalizer</h1>
+        </div>
+        <div className="flex flex-row">
+          <div>Create Signal</div>
+        </div>
+      </main>
+    </>
   );
 }
