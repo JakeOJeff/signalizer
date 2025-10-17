45e4599c267e3eee2d88d7cc5b56f74897fd83f0
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 00:01:34 +0530

    Structuring out the grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      
+      <div></div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
     </main>
   );
 }
