924dcfb3e5d239c76f8527de2ac8e68d1c3f9555
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 00:29:09 +0530

    Created two buttons

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ export default function Home() {
         <div>
           <h1 className="text-6xl p-10">Signalizer</h1>
         </div>
-        <div className="flex flex-row">
-          <div>Create Signal</div>
+        <div className="flex flex-row justify-between ">
+          <div className="rounded-2xl mr-2 outline-3 outline-gray-500 bg-gray-600 p-4 pr-6 pl-6 text-xl font-bold cursor-pointer duration-500 hover:bg-gray-700" >Create Signal</div>
+          <div className="rounded-2xl ml-2 outline-3 outline-gray-300 text-gray-800 bg-gray-100 p-4 pr-6 pl-6 text-xl font-bold cursor-pointer duration-500 hover:bg-gray-300" >Decode Signal</div>
         </div>
       </main>
     </>
