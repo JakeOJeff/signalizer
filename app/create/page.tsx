116504fe03c2dbe9bb9fe45c417a18fe4809f3c0
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 10:49:52 +0530

    Started work on create

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useState, ChangeEvent } from 'react';
 
-export default function create(){
+
+export default function create() {
+    const [name, setName] = useState<string>("");
     return (
         <>
-            <main>
-                create signal
-            </main>
+            <main className="bg-gray-900 flex flex-col items-center justify-center min-h-screen w-full">
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-7xl my-4">Signalizer</h1>
+                    <h3 className="text-2xl mb-15">Enter/Paste the message you need to encode.</h3>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                
+                </div>
+                </main>
         </>
     );
 }
