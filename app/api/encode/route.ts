356ec3102bc73cbb2a356d15e03424acb025e170
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 23:13:50 +0530

    Created error clauses to catch

--- app/api/encode/route.ts
+++ app/api/encode/route.ts
@@ -5,7 +5,18 @@ import { encodeMessage } from "@/app/utils/encodeMessage";
 export async function POST(req:Request) {
     try {
         const { message, method } = await req.json();
+
+        if (!message || !method) {
+            return NextResponse.json(
+                { error: "Missing Message or Method" },
+                { status: 400 }
+            )
+        }
     } catch (error) {
         console.error("Encode API error:", error)
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        )
     }
 }
