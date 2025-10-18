c11bddf4bd132ffba675245fc4b8e5c97528bb39
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 21:54:52 +0530

    Replace MD5 with SHA-256

--- app/utils/encodeMessage.ts
+++ app/utils/encodeMessage.ts
@@ -12,7 +12,7 @@ export async function encodeMessage(msg: string, method: string): Promise<string
         case "md5": {
             const encoder = new TextEncoder();
             const data = encoder.encode(msg);
-            const hashBuffer = await crypto.subtle.digest("MD5", data);
+            const hashBuffer = await crypto.subtle.digest("SHA-256", data);
             const hashArray = Array.from(new Uint8Array(hashBuffer));
             result = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
         }
