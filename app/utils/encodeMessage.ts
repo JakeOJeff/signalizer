046e1d3e1d69a7982274f4775793260031775882
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-18 23:27:20 +0530

    Successfully made the API endpoint

--- app/utils/encodeMessage.ts
+++ app/utils/encodeMessage.ts
@@ -9,12 +9,13 @@ export async function encodeMessage(msg: string, method: string): Promise<string
 
     switch (method) {
 
-        case "sha-256": {
+        case "SHA-256": {
             const encoder = new TextEncoder();
             const data = encoder.encode(msg);
             const hashBuffer = await crypto.subtle.digest("SHA-256", data);
             const hashArray = Array.from(new Uint8Array(hashBuffer));
             result = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+            break;
         }
 
     }
