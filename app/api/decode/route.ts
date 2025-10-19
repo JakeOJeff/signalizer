402fb2a627aeaf8625fde5a147398a8f29f99a74
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-19 15:38:03 +0530

    Successfully routed the decode api

---
+++ app/api/decode/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { decodeMessage } from "@/app/utils/decodeMEssage";
+
+// curl -X POST http://localhost:3000/api/decode  -H "Content-Type: application/json"  -d "{\"message\":\"aGVsbG8=\",\"method\":\"base64\"}"
+export async function POST(req:Request) {
+    try {
+        const { message, method } = await req.json() 
+        const decoded = await decodeMessage(message, method);
+    
+        return NextResponse.json({
+            decoded,
+            reversible: decoded !== "Encryped values cannot be decrypted. Try Brute-Force"
+        });
+    
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500});
+    }
+}
